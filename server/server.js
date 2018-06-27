3150d5f5ab4633eb833bc76f2f2049d3e67eddcc
Uri Rahimi <urirahimi@Uris-MacBook-Pro.local>
2018-06-27 14:20:22 -0700

    emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-// console.log(__dirname + '../public');
-const path = require('path'); 
+const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
@@ -13,33 +12,39 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log('New user connected');
 
-    // socket.emit('newEmail', {
-    //     from: 'Mike@example.com',
-    //     text: 'Suck my dick',
-    //     createAt: 123
-    // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-    socket.emit('newMessage', {
-        from: 'Mike@example.com',
-        text: 'Suck my dick',
-        createdAt: 123
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+  });
 
-    socket.on('createMessage', (message) => {
-        console.log('create message: ', message);
-    })
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
 
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // })
-
-    socket.on('disconnect', function() {
-        console.log('User disconnected');
-    })
-})
-  server.listen(3000, () => {
-      console.log('server is up on port ' + port);
-  })
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
